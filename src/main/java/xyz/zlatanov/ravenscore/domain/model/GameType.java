f2a27e282395e9a78ca05b157f1e5ab06e5f1118
Boyan Zlatanov <b.zlatanov@tinqin.com>
2024-12-06 10:04:30 +0000

    Setup domain model

---
+++ src/main/java/xyz/zlatanov/ravenscore/domain/model/GameType.java
@@ -0,0 +1,6 @@
+package xyz.zlatanov.ravenscore.domain.model;
+
+public enum GameType {
+
+	SECOND_EDITION, A_FEAST_FOR_CROWS, A_DANCE_WITH_DRAGONS, MOTHER_OF_DRAGONS
+}
---
+++ src/main/java/xyz/zlatanov/ravenscore/domain/model/House.java
@@ -0,0 +1,6 @@
+package xyz.zlatanov.ravenscore.domain.model;
+
+public enum House {
+
+	STARK, GREYJOY, LANNISTER, TYRELL, MARTELL, BARATHEON, ARRYN, TARGARYEN
+}
