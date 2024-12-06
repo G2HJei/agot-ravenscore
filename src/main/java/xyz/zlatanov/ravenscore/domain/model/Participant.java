357779b20d8d4f93ef74878a53463746b1827fd5
Boyan Zlatanov <boyan@zlatanov.xyz>
2024-12-06 11:58:49 +0000

    Update ToString. Start working on test data

--- src/main/java/xyz/zlatanov/ravenscore/domain/model/Participant.java
+++ src/main/java/xyz/zlatanov/ravenscore/domain/model/Participant.java
@@ -10,6 +10,7 @@ import org.hibernate.annotations.UuidGenerator;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.ToString;
 import lombok.experimental.Accessors;
 
 @Data
@@ -32,17 +33,21 @@ public class Participant {
 
 	@OneToOne
 	@JoinColumn(name = "replacement_participant_id")
+	@ToString.Exclude
 	private Participant				replacementParticipant;
 
 	@OneToMany(cascade = ALL)
 	@JoinColumn(name = "participant_id")
+	@ToString.Exclude
 	private List<Player>			playerList			= new ArrayList<>();
 
 	@ManyToMany(cascade = { PERSIST, MERGE })
 	@JoinTable(name = "participant_tournament_stage", joinColumns = @JoinColumn(name = "participant_id"), inverseJoinColumns = @JoinColumn(name = "tournament_stage_id"))
+	@ToString.Exclude
 	private List<TournamentStage>	tournamentStageList	= new ArrayList<>();
 
 	@ManyToMany(cascade = { PERSIST, MERGE })
 	@JoinTable(name = "participant_game", joinColumns = @JoinColumn(name = "participant_id"), inverseJoinColumns = @JoinColumn(name = "game_id"))
+	@ToString.Exclude
 	private List<Game>				gameList			= new ArrayList<>();
 }
