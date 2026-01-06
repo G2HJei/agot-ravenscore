33e083732f4b555b7f452f01f5abfc1e721a1a17
Boyan Zlatanov <boyan@zlatanov.xyz>
2026-01-06 08:30:43 +0000

    Handle IN_LOBBY game updates

--- src/main/java/xyz/zlatanov/ravenscore/service/GameAdminService.java
+++ src/main/java/xyz/zlatanov/ravenscore/service/GameAdminService.java
@@ -1,17 +1,12 @@
 package xyz.zlatanov.ravenscore.service;
 
-import static xyz.zlatanov.ravenscore.model.swordsandravens.SnrState.FINISHED;
-
-import java.util.*;
-
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
-
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.val;
-import lombok.extern.slf4j.Slf4j;
 import xyz.zlatanov.ravenscore.domain.model.Game;
 import xyz.zlatanov.ravenscore.domain.model.House;
 import xyz.zlatanov.ravenscore.domain.model.Participant;
@@ -26,210 +21,214 @@ import xyz.zlatanov.ravenscore.model.tourdetails.admin.PlayerRanking;
 import xyz.zlatanov.ravenscore.model.tourdetails.admin.RankingsForm;
 import xyz.zlatanov.ravenscore.security.TournamentAdminOperation;
 
+import java.util.*;
+
+import static xyz.zlatanov.ravenscore.model.swordsandravens.SnrState.FINISHED;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class GameAdminService {
 
-	private final GameRepository		gameRepository;
-	private final PlayerRepository		playerRepository;
-	private final ParticipantRepository	participantRepository;
+    private final GameRepository gameRepository;
+    private final PlayerRepository playerRepository;
+    private final ParticipantRepository participantRepository;
 
-	@Transactional
-	@TournamentAdminOperation
-	public void game(@Valid GameForm gameForm) {
-		validateParticipantCount(gameForm);
-		if (gameForm.getId() == null) {
-			createGame(gameForm);
-		} else {
-			updateGame(gameForm);
-		}
-	}
+    @Transactional
+    @TournamentAdminOperation
+    public void game(@Valid GameForm gameForm) {
+        validateParticipantCount(gameForm);
+        if (gameForm.getId() == null) {
+            createGame(gameForm);
+        } else {
+            updateGame(gameForm);
+        }
+    }
 
-	@Transactional
-	@TournamentAdminOperation
-	public void removeGame(UUID gameId) {
-		val game = gameRepository.findById(gameId).orElseThrow();
-		if (game.completed()) {
-			throw new RavenscoreException("Cannot delete a completed game.");
-		}
-		gameRepository.deleteById(gameId);
-	}
+    @Transactional
+    @TournamentAdminOperation
+    public void removeGame(UUID gameId) {
+        val game = gameRepository.findById(gameId).orElseThrow();
+        if (game.completed()) {
+            throw new RavenscoreException("Cannot delete a completed game.");
+        }
+        gameRepository.deleteById(gameId);
+    }
 
-	@Transactional
-	@TournamentAdminOperation
-	public void updateRankings(@Valid RankingsForm rankingsForm) throws RavenscoreException {
-		validateRankingsForm(rankingsForm);
+    @Transactional
+    @TournamentAdminOperation
+    public void updateRankings(@Valid RankingsForm rankingsForm) throws RavenscoreException {
+        validateRankingsForm(rankingsForm);
 
-		val game = gameRepository.findById(rankingsForm.getGameId()).orElseThrow();
-		gameRepository.save(game.completed(rankingsForm.getCompleted()));
-		val playerScoringList = rankingsForm.getPlayerRankingList().stream()
-				.sorted(Comparator.comparing(PlayerRanking::getPoints))
-				.toList();
-		playerScoringList.forEach(ps -> {
-			val player = playerRepository.findById(ps.getPlayerId()).orElseThrow();
-			playerRepository.save(player
-					.castles(ps.getCastles())
-					.penaltyPoints(ps.getPenaltyPoints())
-					.points(ps.getPoints())
-					.participantId(ps.getParticipantId()));
-		});
-	}
+        val game = gameRepository.findById(rankingsForm.getGameId()).orElseThrow();
+        gameRepository.save(game.completed(rankingsForm.getCompleted()));
+        val playerScoringList = rankingsForm.getPlayerRankingList().stream()
+                .sorted(Comparator.comparing(PlayerRanking::getPoints))
+                .toList();
+        playerScoringList.forEach(ps -> {
+            val player = playerRepository.findById(ps.getPlayerId()).orElseThrow();
+            playerRepository.save(player
+                    .castles(ps.getCastles())
+                    .penaltyPoints(ps.getPenaltyPoints())
+                    .points(ps.getPoints())
+                    .participantId(ps.getParticipantId()));
+        });
+    }
 
-	@Transactional
-	@TournamentAdminOperation
-	public void updateRound(UUID gameId, Integer round) {
-		val game = gameRepository.findById(gameId).orElseThrow();
-		gameRepository.save(game.round(round));
-	}
+    @Transactional
+    @TournamentAdminOperation
+    public void updateRound(UUID gameId, Integer round) {
+        val game = gameRepository.findById(gameId).orElseThrow();
+        gameRepository.save(game.round(round));
+    }
 
-	private void validateParticipantCount(@Valid GameForm gameForm) {
-		val maxNumberOfParticipants = gameForm.getType().houses().size();
-		val selectedParticipants = gameForm.getParticipantIdList().length;
-		if (selectedParticipants > maxNumberOfParticipants) {
-			throw new RavenscoreException(
-					String.format("Maximum number of participants for this type of game is %s.", maxNumberOfParticipants));
-		}
-	}
+    private void validateParticipantCount(@Valid GameForm gameForm) {
+        val maxNumberOfParticipants = gameForm.getType().houses().size();
+        val selectedParticipants = gameForm.getParticipantIdList().length;
+        if (selectedParticipants > maxNumberOfParticipants) {
+            throw new RavenscoreException(
+                    String.format("Maximum number of participants for this type of game is %s.", maxNumberOfParticipants));
+        }
+    }
 
-	private void createGame(GameForm gameForm) {
-		val game = gameRepository.save(new Game()
-				.type(gameForm.getType())
-				.name(gameForm.getName())
-				.link(gameForm.getLink())
-				.completed(false)
-				.tournamentStageId(gameForm.getStageId())
-				.participantIdList(gameForm.getParticipantIdList()));
-		val defaultPlayers = gameForm.getType().houses()
-				.stream()
-				.map(house -> new Player()
-						.gameId(game.id())
-						.house(house))
-				.toList();
-		playerRepository.saveAll(defaultPlayers);
-	}
+    private void createGame(GameForm gameForm) {
+        val game = gameRepository.save(new Game()
+                .type(gameForm.getType())
+                .name(gameForm.getName())
+                .link(gameForm.getLink())
+                .completed(false)
+                .tournamentStageId(gameForm.getStageId())
+                .participantIdList(gameForm.getParticipantIdList()));
+        val defaultPlayers = gameForm.getType().houses()
+                .stream()
+                .map(house -> new Player()
+                        .gameId(game.id())
+                        .house(house))
+                .toList();
+        playerRepository.saveAll(defaultPlayers);
+    }
 
-	private void updateGame(GameForm gameForm) {
-		val game = gameRepository.findById(gameForm.getId()).orElseThrow();
-		if (game.completed()) {
-			throw new RavenscoreException("Cannot update game details of a completed game.");
-		}
-		gameRepository.save(game
-				.type(gameForm.getType())
-				.name(gameForm.getName())
-				.link(gameForm.getLink())
-				.tournamentStageId(gameForm.getStageId())
-				.participantIdList(gameForm.getParticipantIdList()));
-		// disassociate player from removed participant
-		val players = playerRepository.findByGameIdOrderByPointsDesc(game.id()).stream()
-				.filter(p -> p.participantId() != null)
-				.filter(p -> !participatesInGames(p.participantId(), List.of(game)))
-				.map(p -> p.participantId(null))
-				.toList();
-		playerRepository.saveAll(players);
-	}
+    private void updateGame(GameForm gameForm) {
+        val game = gameRepository.findById(gameForm.getId()).orElseThrow();
+        if (game.completed()) {
+            throw new RavenscoreException("Cannot update game details of a completed game.");
+        }
+        gameRepository.save(game
+                .type(gameForm.getType())
+                .name(gameForm.getName())
+                .link(gameForm.getLink())
+                .tournamentStageId(gameForm.getStageId())
+                .participantIdList(gameForm.getParticipantIdList()));
+        // disassociate player from removed participant
+        val players = playerRepository.findByGameIdOrderByPointsDesc(game.id()).stream()
+                .filter(p -> p.participantId() != null)
+                .filter(p -> !participatesInGames(p.participantId(), List.of(game)))
+                .map(p -> p.participantId(null))
+                .toList();
+        playerRepository.saveAll(players);
+    }
 
-	private boolean participatesInGames(UUID participantId, List<Game> games) {
-		return games.stream().flatMap(g -> Arrays.stream(g.participantIdList())).anyMatch(pId -> pId.equals(participantId));
-	}
+    private boolean participatesInGames(UUID participantId, List<Game> games) {
+        return games.stream().flatMap(g -> Arrays.stream(g.participantIdList())).anyMatch(pId -> pId.equals(participantId));
+    }
 
-	private void validateRankingsForm(RankingsForm rankingsForm) {
-		if (rankingsForm.getCompleted() && hasUnrevealedPlayers(rankingsForm)) {
-			throw new RavenscoreException("Cannot complete game without revealing all players!");
-		}
-		if (hasDuplicatedParticipants(rankingsForm)) {
-			throw new RavenscoreException("Same player selected for more than one house!");
-		}
-	}
+    private void validateRankingsForm(RankingsForm rankingsForm) {
+        if (rankingsForm.getCompleted() && hasUnrevealedPlayers(rankingsForm)) {
+            throw new RavenscoreException("Cannot complete game without revealing all players!");
+        }
+        if (hasDuplicatedParticipants(rankingsForm)) {
+            throw new RavenscoreException("Same player selected for more than one house!");
+        }
+    }
 
-	private boolean hasUnrevealedPlayers(RankingsForm rankingsForm) {
-		return rankingsForm.getPlayerRankingList().stream()
-				.map(PlayerRanking::getParticipantId)
-				.anyMatch(Objects::isNull);
-	}
+    private boolean hasUnrevealedPlayers(RankingsForm rankingsForm) {
+        return rankingsForm.getPlayerRankingList().stream()
+                .map(PlayerRanking::getParticipantId)
+                .anyMatch(Objects::isNull);
+    }
 
-	private boolean hasDuplicatedParticipants(RankingsForm rankingsForm) {
-		val participantIdList = rankingsForm.getPlayerRankingList().stream()
-				.map(PlayerRanking::getParticipantId)
-				.filter(Objects::nonNull)
-				.toList();
-		val distinctParticipantIdSize = participantIdList.stream().distinct().toList().size();
-		return participantIdList.size() != distinctParticipantIdSize;
-	}
+    private boolean hasDuplicatedParticipants(RankingsForm rankingsForm) {
+        val participantIdList = rankingsForm.getPlayerRankingList().stream()
+                .map(PlayerRanking::getParticipantId)
+                .filter(Objects::nonNull)
+                .toList();
+        val distinctParticipantIdSize = participantIdList.stream().distinct().toList().size();
+        return participantIdList.size() != distinctParticipantIdSize;
+    }
 
-	public GameUpdates refreshSnrGame(UUID gameId) {
-		val game = gameRepository.findById(gameId)
-				.orElseThrow(() -> new RavenscoreException("Game not found"));
-		val isSnrGame = game.link().toLowerCase().contains("swordsandravens.net");
-		if (!isSnrGame) {
-			throw new RavenscoreException("This game is not a Swords & Ravens game");
-		}
-		val snrGameState = getSnrGameState(game);
-		return checkForUpdates(game, snrGameState);
-	}
+    public GameUpdates refreshSnrGame(UUID gameId) {
+        val game = gameRepository.findById(gameId)
+                .orElseThrow(() -> new RavenscoreException("Game not found"));
+        val isSnrGame = game.link().toLowerCase().contains("swordsandravens.net");
+        if (!isSnrGame) {
+            throw new RavenscoreException("This game is not a Swords & Ravens game");
+        }
+        val snrGameState = getSnrGameState(game);
+        return checkForUpdates(game, snrGameState);
+    }
 
-	private SnrGameState getSnrGameState(Game game) {
-		val snrGameId = game.link()
-				.replaceAll(".*swordsandravens\\.net/play/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*", "$1");
-		return WebClient.create()
-				.get()
-				.uri("https://swordsandravens.net/api/public/game/" + snrGameId + "?format=json")
-				.retrieve()
-				.bodyToMono(SnrGameState.class)
-				.block();
-	}
+    private SnrGameState getSnrGameState(Game game) {
+        val snrGameId = game.link()
+                .replaceAll(".*swordsandravens\\.net/play/([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}).*", "$1");
+        return WebClient.create()
+                .get()
+                .uri("https://swordsandravens.net/api/public/game/" + snrGameId + "?format=json")
+                .retrieve()
+                .bodyToMono(SnrGameState.class)
+                .block();
+    }
 
-	private GameUpdates checkForUpdates(Game game, SnrGameState snrGameState) {
-		val snrRound = snrGameState.viewOfGame().round();
-		val snrCompleted = snrGameState.state() == FINISHED;
-		return new GameUpdates()
-				.gameId(game.id())
-				.gameName(game.name())
-				.round(!game.round().equals(snrRound) ? snrRound : null)
-				.completed(snrCompleted ? true : null)
-				.playerRankingList(snrCompleted ? getPlayerRankings(game, snrGameState) : null);
-	}
+    private GameUpdates checkForUpdates(Game game, SnrGameState snrGameState) {
+        val snrRound = snrGameState.viewOfGame().round();
+        val snrCompleted = snrGameState.state() == FINISHED;
+        return new GameUpdates()
+                .gameId(game.id())
+                .gameName(game.name())
+                .round(snrRound > 0 && !snrRound.equals(game.round()) ? snrRound : null)
+                .completed(snrCompleted ? true : null)
+                .playerRankingList(snrCompleted ? getPlayerRankings(game, snrGameState) : null);
+    }
 
-	private ArrayList<PlayerRanking> getPlayerRankings(Game game, SnrGameState snrGameState) {
-		val playerRankingList = new ArrayList<PlayerRanking>();
-		val players = playerRepository.findByGameIdOrderByPointsDesc(game.id());
-		val participants = participantRepository.findByIdInOrderByName(Arrays.stream(game.participantIdList()).toList());
-		val victoryTrack = snrGameState.viewOfGame().victoryTrack();
-		for (int rank = 1; rank <= victoryTrack.size(); rank++) {
-			val snrPlayer = victoryTrack.get(rank - 1);
-			val player = findPlayer(players, snrPlayer.house());
-			val participant = findParticipant(participants, snrPlayer.name());
-			playerRankingList.add(
-					new PlayerRanking()
-							.setPlayerId(player.id())
-							.setHouse(player.house().label())
-							.setParticipantId(participant.id())
-							.setParticipantName(participant.name())
-							.setPoints(victoryTrack.size() - rank + snrPlayer.victoryPoints() + (rank == 1 ? 3 : 0))
-							.setCastles(snrPlayer.victoryPoints())
-							.setPenaltyPoints(player.penaltyPoints()));
-		}
-		return playerRankingList;
-	}
+    private ArrayList<PlayerRanking> getPlayerRankings(Game game, SnrGameState snrGameState) {
+        val playerRankingList = new ArrayList<PlayerRanking>();
+        val players = playerRepository.findByGameIdOrderByPointsDesc(game.id());
+        val participants = participantRepository.findByIdInOrderByName(Arrays.stream(game.participantIdList()).toList());
+        val victoryTrack = snrGameState.viewOfGame().victoryTrack();
+        for (int rank = 1; rank <= victoryTrack.size(); rank++) {
+            val snrPlayer = victoryTrack.get(rank - 1);
+            val player = findPlayer(players, snrPlayer.house());
+            val participant = findParticipant(participants, snrPlayer.name());
+            playerRankingList.add(
+                    new PlayerRanking()
+                            .setPlayerId(player.id())
+                            .setHouse(player.house().label())
+                            .setParticipantId(participant.id())
+                            .setParticipantName(participant.name())
+                            .setPoints(victoryTrack.size() - rank + snrPlayer.victoryPoints() + (rank == 1 ? 3 : 0))
+                            .setCastles(snrPlayer.victoryPoints())
+                            .setPenaltyPoints(player.penaltyPoints()));
+        }
+        return playerRankingList;
+    }
 
-	private static Participant findParticipant(List<Participant> participants, String playerName) {
-		return participants.stream()
-				.filter(p -> hasSameNameOrAlias(p, playerName))
-				.findFirst()
-				.orElseThrow();
-	}
+    private static Participant findParticipant(List<Participant> participants, String playerName) {
+        return participants.stream()
+                .filter(p -> hasSameNameOrAlias(p, playerName))
+                .findFirst()
+                .orElseThrow();
+    }
 
-	private static boolean hasSameNameOrAlias(Participant participant, String playerName) {
-		val playerNameMatch = participant.name().equalsIgnoreCase(playerName);
-		val aliasMatch = Arrays.stream(participant.profileLinks())
-				.anyMatch(pl -> pl.equalsIgnoreCase(playerName));
-		return playerNameMatch || aliasMatch;
-	}
+    private static boolean hasSameNameOrAlias(Participant participant, String playerName) {
+        val playerNameMatch = participant.name().equalsIgnoreCase(playerName);
+        val aliasMatch = Arrays.stream(participant.profileLinks())
+                .anyMatch(pl -> pl.equalsIgnoreCase(playerName));
+        return playerNameMatch || aliasMatch;
+    }
 
-	private static Player findPlayer(List<Player> players, House house) {
-		return players.stream()
-				.filter(p -> p.house() == house)
-				.findFirst()
-				.orElseThrow();
-	}
+    private static Player findPlayer(List<Player> players, House house) {
+        return players.stream()
+                .filter(p -> p.house() == house)
+                .findFirst()
+                .orElseThrow();
+    }
 }
